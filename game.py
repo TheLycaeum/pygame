0f4a75ec08c4b2176138d0fd71ec6a5ce956fcf4
Noufal Ibrahim <noufal@nibrahim.net.in>
2014-09-02 15:19:33 +0530

    Create screen

---
+++ game.py
@@ -0,0 +1,11 @@
+import pygame
+from pygame.locals import Rect, DOUBLEBUF
+
+
+def main():
+    screen = pygame.display.set_mode((800,600), DOUBLEBUF)
+    
+
+if __name__ == '__main__':
+    main()
+
