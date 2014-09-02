dae9f43ac9cd9c13606a3e8c6a2b75c80e3863c3
Noufal Ibrahim <noufal@nibrahim.net.in>
2014-09-02 15:22:02 +0530

    Empty event loop.

--- game.py
+++ game.py
@@ -4,6 +4,8 @@ from pygame.locals import Rect, DOUBLEBUF
 
 def main():
     screen = pygame.display.set_mode((800,600), DOUBLEBUF)
+    while True:
+        pass
     
 
 if __name__ == '__main__':
