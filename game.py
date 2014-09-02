07a74199f84ed53354f9b22bac7ecec95fbf975e
Noufal Ibrahim <noufal@nibrahim.net.in>
2014-09-02 15:44:43 +0530

    Adds a starfield.

--- game.py
+++ game.py
@@ -1,11 +1,56 @@
+import random
+
 import pygame
 from pygame.locals import Rect, DOUBLEBUF
 
+X_MAX = 800
+Y_MAX = 600
+
+class Star(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super(Star,self).__init__()
+        self.image = pygame.Surface((3,3))
+        pygame.draw.circle(self.image, 
+                           (255, 255, 255), 
+                           (0,0),
+                           3,
+                           0)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+    
+    def update(self):
+        x, y = self.rect.center
+        if self.rect.center[1] > Y_MAX:
+            self.rect.center = (x, 0)
+        else:
+            self.rect.center = (x, y + 5)
+                           
+
+def create_starfield():
+    starfield = pygame.sprite.Group()
+    for i in range(100):
+        x,y = random.randrange(X_MAX), random.randrange(Y_MAX)
+        s = Star(x, y)
+        s.add(starfield)
+        
+    return starfield
+
+
 
 def main():
-    screen = pygame.display.set_mode((800,600), DOUBLEBUF)
+    screen = pygame.display.set_mode((X_MAX, Y_MAX), DOUBLEBUF)
+    empty = pygame.Surface((X_MAX, Y_MAX))
+    clock = pygame.time.Clock()
+
+    starfield = create_starfield()
     while True:
-        pass
+        clock.tick(30)
+
+        starfield.clear(screen, empty)
+        starfield.update()
+        starfield.draw(screen)
+        pygame.display.flip()
+
     
 
 if __name__ == '__main__':
