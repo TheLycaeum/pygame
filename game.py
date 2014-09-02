2f027a0254d302cb436ad0c8fee88d3cdf7f467f
Noufal Ibrahim <noufal@nibrahim.net.in>
2014-09-02 16:39:39 +0530

    Adds simple motion to ship

--- game.py
+++ game.py
@@ -2,10 +2,12 @@ import random
 import sys
 
 import pygame
-from pygame.locals import Rect, DOUBLEBUF, QUIT, K_ESCAPE, KEYDOWN
+from pygame.locals import Rect, DOUBLEBUF, QUIT, K_ESCAPE, KEYDOWN, K_DOWN, K_LEFT, K_UP, K_RIGHT
 
 X_MAX = 800
 Y_MAX = 600
+
+LEFT, RIGHT, UP, DOWN = 0, 1, 3, 4
 
 class Star(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -37,6 +39,17 @@ class ShipSprite(pygame.sprite.Sprite):
     def update(self):
         pass
 
+    def move(self, direction):
+        v = 1
+        dx, dy = {UP : (0, -v),
+                  DOWN : (0, v),
+                  LEFT : (-v, 0),
+                  RIGHT : (v, 0)}[direction]
+
+        x, y = self.rect.center
+        self.rect.center = x + dx, y + dy
+
+
 def create_starfield(group):
     for i in range(100):
         x,y = random.randrange(X_MAX), random.randrange(Y_MAX)
@@ -62,6 +75,18 @@ def main():
         for event in pygame.event.get():
             if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
                 sys.exit()
+
+            if event.type == KEYDOWN:
+                if event.key == K_DOWN:  
+                    ship.move(DOWN)
+                if event.key == K_LEFT:  
+                    ship.move(LEFT)
+                if event.key == K_RIGHT: 
+                    ship.move(RIGHT)
+                if event.key == K_UP:    
+                    ship.move(UP)
+
+            
 
         # Update sprites
         everything.clear(screen, empty)
